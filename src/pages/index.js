f7675961fc94fd683ef3602944a1803b885dd193
Ayana Osawa <>
2019-07-08 15:37:46 -0400

    added script tag to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         <SEO title="All posts" />
         <Bio />
         {posts.map(({ node }) => {
