14b6efdf4d07aa45845493cdb7aa514a62a93057
Ayana Osawa <>
2019-07-08 16:03:01 -0400

    updated config & title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Ayana's Blog`,
-    author: `Ayana`,
+    title: `Ayaos Blog`,
+    author: `Ayaos`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
