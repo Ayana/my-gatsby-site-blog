9725c3d68e1546ea273578fc2ec4be1ffaf3c084
Ayana Osawa <>
2019-07-08 22:54:21 -0400

    updated bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -56,7 +56,7 @@ const Bio = () => {
       <p>
         <strong>{author}</strong>
         {` `}
-        <a target="_blank" href={`https://twitter.com/${social.twitter}`}></a>
+        <a target="_blank" href={`https://twitter.com/AyaosEats`}>
           Twitter
         </a>
       </p>
