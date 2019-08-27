1d1942586193e4b44fd6e301a650b68ddd435d85
Ayana Osawa <>
2019-08-27 11:42:17 -0400

    removed unnecessary import code

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,7 +4,6 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
-import Image from "gatsby-image"
 
 class BlogPostTemplate extends React.Component {
   render() {
