1cabb6dca543138876dda5f9c9fa35d7e49fbc90
Ayana Osawa <>
2019-09-28 14:43:15 -0400

    Add searchbar & youtube api fetch

---
+++ src/components/app/SearchBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' }
+
+  onInputChange = (event) => {
+    this.setState({
+      term: event.target.value 
+    })
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+    this.props.onFormSubmit(this.state.term)
+  }
+
+  render() { 
+    return (
+      <div className="search-bar ui segment" style={{marginTop: "2em"}}>
+        <form className="ui search" onSubmit={this.onFormSubmit}>
+          <div className="ui icon input fluid">
+            <input 
+              className="prompt" 
+              type="text" 
+              placeholder="Search videos..." 
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+
+        </form>
+      </div>
+    )
+  }
+}
+ 
+export default SearchBar;
