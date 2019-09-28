928a7a09dc1a41a15e86d70b4b0af420c8cdaa22
Ayana Osawa <>
2019-09-28 15:13:39 -0400

    Add all function for video player

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -25,6 +25,10 @@ class App extends React.Component {
 
   state = { videos: [], selectedVideo: null }
 
+  componentDidMount() {
+    this.onTermSubmit('cooking')
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
@@ -32,7 +36,14 @@ class App extends React.Component {
       }
     })
 
-    this.setState({ videos: response.data.items })
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
+  }
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video })
   }
 
   render() { 
@@ -40,17 +51,18 @@ class App extends React.Component {
       <AppStyle>
         <div className="ui container">
           <SearchBar onFormSubmit={this.onTermSubmit} />
+          I have {this.state.videos.length} videos.
 
           <div className="row">
-            {/* <div className="eleven wide column">
+            <div className="eleven wide column">
               <VideoDetail video={this.state.selectedVideo}/>
-            </div> */}
-            {/* <div className="five wide column">
+            </div>
+            <div className="five wide column">
               <VideoList
                 onVideoSelect={this.onVideoSelect}
                 videos={this.state.videos} 
               />
-            </div> */}
+            </div>
           </div>
 
         </div>
