b493b32f2d2a25c41ed921da0c4a70321c6f0f0a
Ayana Osawa <>
2019-12-29 16:55:42 -0500

    Add conditional function

--- src/components/demo/Increment.js
+++ src/components/demo/Increment.js
@@ -25,19 +25,37 @@ const StyledIncrement = styled.div`
 const Increment = () => {
   const [ count, setCount ] = useState(0)
 
+  function funcDecrement() {
+    if (count > 0) {
+      setCount(count - 1)
+    } else if (count === 'Over') {
+      setCount(10)
+    } else {
+
+    }
+  }
+
+  function funcIncrement () {
+    if (count < 10) {
+      setCount(count + 1)
+    } else {
+      setCount('Over')
+    }
+  }
+
   return (
     <StyledIncrement>
       <div className="counter-box">
         <div className="number">{count}</div>
         <button
           className="button inc"
-          onClick={ () => setCount( count - 1 ) }
+          onClick={funcDecrement}
         >
           -
         </button>
         <button
           className="button inc"
-          onClick={ () => setCount( count + 1 ) }
+          onClick={funcIncrement}
         >
           +
         </button>
@@ -53,5 +71,37 @@ const Increment = () => {
     </StyledIncrement>
   )
 }
+
+// const Increment = () => {
+//   const [ count, setCount ] = useState(0)
+
+//   return (
+//     <StyledIncrement>
+//       <div className="counter-box">
+//         <div className="number">{count}</div>
+//         <button
+//           className="button inc"
+//           onClick={ () => setCount( count - 1 ) }
+//         >
+//           -
+//         </button>
+//         <button
+//           className="button inc"
+//           onClick={ () => setCount( count + 1 ) }
+//         >
+//           +
+//         </button>
+//         <div>
+//           <button
+//             className="button tiny hollow"
+//             onClick={ () => setCount(0) }
+//           >
+//             Reset
+//           </button>
+//         </div>
+//       </div>
+//     </StyledIncrement>
+//   )
+// }
 
 export default Increment
