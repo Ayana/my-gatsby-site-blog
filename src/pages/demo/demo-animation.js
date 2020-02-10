c17b5bccd9d50e23b507ce9dbfc6ae019644d1f1
Ayana Osawa <ayana.osawa@gmail.com>
2020-02-10 18:30:40 -0500

    Fix bike animation

--- src/pages/demo/demo-animation.js
+++ src/pages/demo/demo-animation.js
@@ -1,70 +1,77 @@
-import React from "react"
-import Layout from "../../components/Layout"
-import SEO from "../../components/SEO"
-import styled from "styled-components"
+import React from 'react';
+import Layout from '../../components/Layout';
+import SEO from '../../components/SEO';
+import styled from 'styled-components';
 
-import Bike from "../../components/demo/Bike"
+import Bike from '../../components/demo/Bike';
 
 const StyledAnimation = styled.div`
-h1 {
-	margin-bottom: 1em;
-}
-.demo-section {
-  margin-bottom: 6em;
-	.effect-wrap {
-		margin: 2em 0;
-	}
-}
-.bike {
-	width: 100%;
-	#pedal,
-	#wheel-right,
-	#wheel-left {
-		animation: Rotate 2s linear infinite;
-		transform-origin: center;
-		transform-box: fill-box;
-	}
-}
-@keyframes Rotate {
-  from {
-    transform: rotate(360deg);
+  h1 {
+    margin-bottom: 1em;
   }
-  to {
-    transform: rotate(0deg);
+  .demo-section {
+    margin-bottom: 6em;
+    .effect-wrap {
+      margin: 2em 0;
+    }
   }
-}
-@media (max-width: 750px) {
-	
-}
-@media (min-width: 751px) {
-}
-`
+  .bike {
+    width: 100%;
+    #front-wheel,
+    #rear-wheel {
+      animation: Rotate 2s linear infinite;
+      transform-origin: center;
+      transform-box: fill-box;
+    }
+    #pedal-right {
+      animation: Rotate 2s linear infinite;
+      transform-origin: 22% 86%;
+      transform-box: fill-box;
+    }
+    #pedal-left {
+      animation: Rotate 2s linear infinite;
+      transform-origin: 78% 14%;
+      transform-box: fill-box;
+    }
+  }
+  @keyframes Rotate {
+    from {
+      transform: rotate(360deg);
+    }
+    to {
+      transform: rotate(0deg);
+    }
+  }
+  @media (max-width: 750px) {
+  }
+  @media (min-width: 751px) {
+  }
+`;
 
-const Animation = props => {
+const Animation = (props) => {
+  const location = props.location;
+  const pageTitle = 'Demo Fun Animations';
+  const pageSlug = 'demo-fun-animations';
 
-  const location = props.location
-  const pageTitle = "Demo Fun Animations"
-  const pageSlug = "demo-fun-animations"
-
-
-	return(
-		<Layout location={location}>
+  return (
+    <Layout location={location}>
       <SEO pageTitle={pageTitle} pageSlug={pageSlug} />
       <StyledAnimation>
-        <div className="container">
+        <div className='container'>
           <h1>{pageTitle}</h1>
 
-					<section className="demo-section">
-						<h2 className="font-script" style={{fontSize: '2.5rem'}}>Let's go biking!</h2>
-						<div className="effect-wrap">
-							<Bike />
-						</div>
-					</section>
-
+          <section className='demo-section'>
+            <h2 className='font-script' style={{ fontSize: '2.5rem' }}>
+              Let's go cycling!
+            </h2>
+            <div className='effect-wrap'>
+              <Bike />
+            </div>
+          </section>
         </div>
       </StyledAnimation>
     </Layout>
-	)
-}
+  );
+};
 
-export default Animation
+export default Animation;
