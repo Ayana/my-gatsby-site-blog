4bd5c024d7230c0085f4f837f37681c51c306c76
Ayana Osawa <ayana.osawa@gmail.com>
2020-04-16 16:38:53 -0400

    Update twitter account, replace nav to demo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,103 +1,103 @@
 module.exports = {
-  siteMetadata: {
-    title: `Ayaos`,
-    author: `Ayana`,
-    description: `Ayaos playground - about web development and fun projects`,
-    siteUrl: `https://ayaos.work/`,
-    image: `/images/img_ogp.jpg`, // have to build after putting images
-    social: {
-      twitter: `AyaosEats`,
-      instagram: `ayaos_eats`,
-      linkedin: `ayana-osawa-22b623b`,
-      github: `Ayana`,
-    },
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/assets/`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `UA-143510928-1`,
-    //   },
-    // },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-PQHKHDP",
-        includeInDevelopment: false,
-        defaultDataLayer: { platform: "gatsby" },
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Ayaos Work`,
-        short_name: `Ayaos`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/assets/images/ayaos-icon.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-styled-components`,
-    },
-    {
-      resolve: `gatsby-plugin-transition-link`,
-    },
-    `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-source-instagram`,
-      options: {
-        username: `misawayanohanashi`,
-      },
-    }, 
-  ],
+	siteMetadata: {
+		title: `Ayaos`,
+		author: `Ayana`,
+		description: `Ayaos playground - about web development and fun projects`,
+		siteUrl: `https://ayaos.work/`,
+		image: `/images/img_ogp.jpg`, // have to build after putting images
+		social: {
+			twitter: `ayanaosawa`,
+			instagram: `ayaos_eats`,
+			linkedin: `ayana-osawa-22b623b`,
+			github: `Ayana`,
+		},
+	},
+	plugins: [
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/blog`,
+				name: `blog`,
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/src/assets/`,
+				name: `assets`,
+			},
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 590,
+						},
+					},
+					{
+						resolve: `gatsby-remark-responsive-iframe`,
+						options: {
+							wrapperStyle: `margin-bottom: 1.0725rem`,
+						},
+					},
+					`gatsby-remark-prismjs`,
+					`gatsby-remark-copy-linked-files`,
+					`gatsby-remark-smartypants`,
+				],
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		// {
+		//   resolve: `gatsby-plugin-google-analytics`,
+		//   options: {
+		//     trackingId: `UA-143510928-1`,
+		//   },
+		// },
+		{
+			resolve: 'gatsby-plugin-google-tagmanager',
+			options: {
+				id: 'GTM-PQHKHDP',
+				includeInDevelopment: false,
+				defaultDataLayer: { platform: 'gatsby' },
+			},
+		},
+		`gatsby-plugin-feed`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Ayaos Work`,
+				short_name: `Ayaos`,
+				start_url: `/`,
+				background_color: `#ffffff`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/assets/images/ayaos-icon.png`,
+			},
+		},
+		`gatsby-plugin-offline`,
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-plugin-typography`,
+			options: {
+				pathToConfigModule: `src/utils/typography`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-styled-components`,
+		},
+		{
+			resolve: `gatsby-plugin-transition-link`,
+		},
+		`gatsby-plugin-sass`,
+		{
+			resolve: `gatsby-source-instagram`,
+			options: {
+				username: `misawayanohanashi`,
+			},
+		},
+	],
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,107 +1,87 @@
-import React from "react"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
-import Logo from "./Logo"
-import styled from "styled-components"
+import React from 'react'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import Logo from './Logo'
+import styled from 'styled-components'
 
 const StyledNav = styled.div`
-  .global-nav {
-    display: flex;
-    justify-content: space-between;
-    .global-nav__logo {
-      margin-top: -0.5em;
-      width: 5em;
-      @media (min-width: 750px) {
-        width: 8em;
-      }
-    }
-    .global-nav__list {
-      display: flex;
-      justify-content: flex-end;
-      .global-nav__link {
-        color: #444;
-        font-family: 'Merriweather', sans-serif;
-        font-size: 0.95rem;
-        text-align: center;
-        margin-left: 1.5em;
-        max-height: 50px;
-        @media (min-width: 768px) {
-          position: relative;
-          display: inline-block;
-          text-decoration: none;
-          height: 22px;
-          margin-left: 2em;
-          &:after {
-            position: absolute;
-            bottom: -1px;
-            left: 0;
-            content: '';
-            width: 100%;
-            height: 1px;
-            background: #888;
-            transform: scale(0, 1);
-            transform-origin: left top;
-            transition: transform .7s cubic-bezier(.10,.7,0,1);
-          }
-          &:hover:after {
-            transform: scale(1, 1);
-          }
-        }
-      }
-    }
-  }
-`;
-
+	.global-nav {
+		display: flex;
+		justify-content: space-between;
+		.global-nav__logo {
+			margin-top: -0.5em;
+			width: 5em;
+			@media (min-width: 750px) {
+				width: 8em;
+			}
+		}
+		.global-nav__list {
+			display: flex;
+			justify-content: flex-end;
+			.global-nav__link {
+				color: #444;
+				font-family: 'Merriweather', sans-serif;
+				font-size: 0.95rem;
+				text-align: center;
+				margin-left: 1.5em;
+				max-height: 50px;
+				@media (min-width: 768px) {
+					position: relative;
+					display: inline-block;
+					text-decoration: none;
+					height: 22px;
+					margin-left: 2em;
+					&:after {
+						position: absolute;
+						bottom: -1px;
+						left: 0;
+						content: '';
+						width: 100%;
+						height: 1px;
+						background: #888;
+						transform: scale(0, 1);
+						transform-origin: left top;
+						transition: transform 0.7s cubic-bezier(0.1, 0.7, 0, 1);
+					}
+					&:hover:after {
+						transform: scale(1, 1);
+					}
+				}
+			}
+		}
+	}
+`
 
 class Nav extends React.Component {
-  render() {
-    return (
-      <StyledNav>
-        <div className="global-nav">
-          <AniLink
-            fade to={`/`} 
-            duration={0.3}
-            className="global-nav__logo"
-          >
-            <Logo />
-          </AniLink>
-          <nav className="global-nav__list">
-            <AniLink 
-              className="global-nav__link"
-              fade 
-              to='/projects/'
-              duration={0.3}
-            >
-              Projects
-            </AniLink>
-            <AniLink 
-              className="global-nav__link"
-              fade 
-              to='/about/' 
-              duration={0.3}
-            >
-              About
-            </AniLink>
-            <AniLink 
+	render() {
+		return (
+			<StyledNav>
+				<div className='global-nav'>
+					<AniLink fade to={`/`} duration={0.3} className='global-nav__logo'>
+						<Logo />
+					</AniLink>
+					<nav className='global-nav__list'>
+						<AniLink className='global-nav__link' fade to='/projects/' duration={0.3}>
+							Projects
+						</AniLink>
+						<AniLink className='global-nav__link' fade to='/about/' duration={0.3}>
+							About
+						</AniLink>
+						{/* <AniLink 
               className="global-nav__link"
               fade 
               to='/blog/' 
               duration={0.3}
             >
               Blog
-            </AniLink>
-            {/* <AniLink 
-              className="global-nav__link"
-              fade 
-              to='/demo/' 
-              duration={0.3}
-            >
-              Demo
             </AniLink> */}
-          </nav>
-        </div>
-      </StyledNav>
-    )
-  }
+						<AniLink className='global-nav__link' fade to='/demo/' duration={0.3}>
+							Demo
+						</AniLink>
+					</nav>
+				</div>
+			</StyledNav>
+		)
+	}
 }
 
 export default Nav
