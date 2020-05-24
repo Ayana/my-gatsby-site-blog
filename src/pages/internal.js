4939f9e153b2c6032a14208e27a520623d033cb4
Ayana Osawa <ayana.osawa@gmail.com>
2020-05-24 15:26:09 -0400

    Add netlify form

--- src/pages/internal.js
+++ src/pages/internal.js
@@ -1,13 +1,13 @@
-import React from "react"
-import Layout from "../components/Layout"
-import SEO from "../components/SEO"
+import React from 'react'
+import Layout from '../components/Layout'
+import SEO from '../components/SEO'
 
 // Classの場合
 // class Internal extends React.Component {
 //   render() {
 //     return (
 // 			// location={this.props.location}の部分は、このページのlocationをLayoutコンポネントに渡している
-//       <Layout location={this.props.location}> 
+//       <Layout location={this.props.location}>
 //         <SEO title="Internal View" />
 //         <div className="container">
 //           <h1>Internal View</h1>
@@ -18,21 +18,31 @@ import SEO from "../components/SEO"
 // }
 
 // Functionの場合
-const Internal = props => {
+const Internal = (props) => {
+	const pageTitle = 'Internal View'
+	const pageSlug = 'internal-view'
 
-  const pageTitle = 'Internal View'
-  const pageSlug = 'internal-view'
-
-  return (
-    <Layout location={props.location}>
-      <SEO pageTitle={pageTitle} pageSlug={pageSlug} />
-      <div className="container">
-        <h1>{pageTitle}</h1>
-      </div>
-    </Layout>
-  )
-
+	return (
+		<Layout location={props.location}>
+			<SEO pageTitle={pageTitle} pageSlug={pageSlug} />
+			<div className="container">
+				<h1>{pageTitle}</h1>
+				<form name="contact" method="post" netlify>
+					<div class="fields">
+						<div class="field">
+							<textarea name="message" rows="4" placeholder="Request message"></textarea>
+						</div>
+						<div class="field">
+							<input type="email" name="email" placeholder="Email" />
+						</div>
+					</div>
+					<div class="actions">
+						<button type="submit">Send</button>
+					</div>
+				</form>
+			</div>
+		</Layout>
+	)
 }
 
 export default Internal
-
