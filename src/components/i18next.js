ad8237c99c6eb8cf5e0db7d038f1430613c04247
Ayana Osawa <ayana.osawa@gmail.com>
2020-06-07 21:42:38 -0400

    Update i18next files

--- src/components/i18next.js
+++ src/components/i18next.js
@@ -1,13 +1,13 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-xhr-backend'
-// import LanguageDetector from 'i18next-browser-languagedetector'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 const Languages = ['en', 'ja']
 
 i18n
 	.use(Backend)
-	// .use(LanguageDetector)
+	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
 		fallbackLng: 'en',
--- src/pages/demo/demo-i18next.js
+++ src/pages/demo/demo-i18next.js
@@ -1,13 +1,11 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import Layout from '../../components/Layout'
 import SEO from '../../components/SEO'
 import styled from 'styled-components'
-import TranslationList from '../../locales/en/translation'
+// import objectsList from '../../utils/objectsList'
 
-// import { useTranslation } from 'react-i18next'
-// import '../../components/i18next'
-
-// import Misawaya from '../../assets/images/demo/misawaya.jpg'
+import '../../components/i18next'
+import { useTranslation } from 'react-i18next'
 
 const StyledWrap = styled.div`
 	.nav {
@@ -30,47 +28,48 @@ function Translation({ location }) {
 	const pageTitle = 'Demo i18next'
 	const pageSlug = 'demo-i18next'
 
-	// const [t, i18n] = useTranslation('translation', { useSuspense: false })
-	// const changeLanguage = (lang) => {
-	// 	i18n.changeLanguage(lang)
-	// }
+	const [t, i18n] = useTranslation('translation', { useSuspense: false })
+	const changeLanguage = (lang) => {
+		i18n.changeLanguage(lang)
+	}
 
-	console.log(TranslationList[0].description[1])
+	// 配列の取得
+	// console.log(objectsList[0].description[1])
 
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<Layout location={location}>
-				<SEO pageTitle={pageTitle} pageSlug={pageSlug} />
-				<StyledWrap className="container basic-link">
-					<div>
-						<h1>{pageTitle}</h1>
+		<Layout location={location}>
+			<SEO pageTitle={pageTitle} pageSlug={pageSlug} />
+			<StyledWrap className="container basic-link">
+				<div>
+					<h1>{pageTitle}</h1>
 
-						{/* <nav className="nav font-typewriter">
-							<button onClick={() => changeLanguage('en')}>En</button>
-							<button onClick={() => changeLanguage('ja')}>Ja</button>
-						</nav> */}
+					<nav className="nav font-typewriter">
+						<button onClick={() => changeLanguage('en')}>En</button>
+						<button onClick={() => changeLanguage('ja')}>Ja</button>
+					</nav>
 
-						<div style={{ marginBottom: '5rem' }}>{/* <h2>{t('heading')}</h2>
-							<div>{t('description.1')}</div>
-							<div>{t('description.2')}</div> */}</div>
-
-						<p>i18next didn't work well.... it was ok in development but got error during build</p>
-
-						<p className="ref">
-							Refference:
-							<br />
-							<a href="https://react.i18next.com/" target="_blank" rel="noopener noreferrer">
-								react-i18next documentation
-							</a>
-							<br />
-							<a href="https://www.youtube.com/watch?v=cHqxgLhOl5Y&t=1s" target="_blank" rel="noopener noreferrer">
-								Multi-language Translate React JS APP with React Hook & I18NEXT(Youtube)
-							</a>
-						</p>
+					<div style={{ marginBottom: '5rem' }}>
+						<h2>{t('heading')}</h2>
+						<div>{t('description.1')}</div>
+						<div>{t('description.2')}</div>
 					</div>
-				</StyledWrap>
-			</Layout>
-		</Suspense>
+
+					<p>i18next didn't work well.... it was ok in development but got error during build</p>
+
+					<p className="ref">
+						Refference:
+						<br />
+						<a href="https://react.i18next.com/" target="_blank" rel="noopener noreferrer">
+							react-i18next documentation
+						</a>
+						<br />
+						<a href="https://www.youtube.com/watch?v=cHqxgLhOl5Y&t=1s" target="_blank" rel="noopener noreferrer">
+							Multi-language Translate React JS APP with React Hook & I18NEXT(Youtube)
+						</a>
+					</p>
+				</div>
+			</StyledWrap>
+		</Layout>
 	)
 }
 
