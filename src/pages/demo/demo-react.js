4144e80fbde0e5f8e79faeb2202a44f4700ccfd3
Ayana Osawa <ayana.osawa@gmail.com>
2020-07-06 16:50:15 -0400

    Add fadeout button

--- src/pages/demo/demo-react.js
+++ src/pages/demo/demo-react.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import Layout from '../../components/Layout'
 import SEO from '../../components/SEO'
 import styled from 'styled-components'
@@ -8,6 +8,23 @@ const StyledWrap = styled.div`
 	.demo-section {
 		padding: 5rem 0;
 		text-align: center;
+	}
+
+	.show__item {
+		min-height: 100px;
+		.show__item__detail {
+			transition: all 0.3s;
+			opacity: 0;
+			&.show {
+				opacity: 1;
+			}
+		}
+		.show__item__button {
+			background: black;
+			color: white;
+			border-radius: 3px;
+			padding: 5px;
+		}
 	}
 	/* @media (max-width: 750px) {
 	} */
@@ -31,9 +48,24 @@ const StyledWrap = styled.div`
 //   }
 // }
 
-function FocusInput({ location }) {
+function FocusInput({location}) {
 	const pageTitle = 'Demo Next'
 	const pageSlug = 'demo-next'
+
+	const [isDetail, setIsDetail] = useState(false)
+	const [isClass, setIsClass] = useState('')
+
+	function handleDisplay(e) {
+		e.currentTarget.classList.add('hide')
+
+		setTimeout(() => {
+			setIsDetail(true)
+
+			setTimeout(() => {
+				setIsClass('show')
+			}, 200)
+		}, 200)
+	}
 
 	const inputRef = useRef(null)
 
@@ -48,6 +80,22 @@ function FocusInput({ location }) {
 	//   navigate(`/${urlLang}`)
 	// }, [])
 
+	// function myClicker() {
+	// 	i++
+	// 	console.log(i)
+	// }
+
+	// useEffect(() => {
+	// var i = 0
+	// const elem = document.getElementById('my-elem')
+	// elem.addEventListener('click', function() {
+	// 	// myClicker()
+	// 	i++
+	// 	elem.value = i
+	// 	console.log(i)
+	// })
+	// }, [])
+
 	return (
 		<Layout location={location}>
 			<SEO pageTitle={pageTitle} pageSlug={pageSlug} />
@@ -57,7 +105,7 @@ function FocusInput({ location }) {
 					<h2>useRef() sample</h2>
 					<input ref={inputRef} type="text" />
 
-					<p style={{ marginTop: '5rem' }}>Will try this next</p>
+					<p style={{marginTop: '5rem'}}>Will try this next</p>
 					<p className="ref">
 						Refference:
 						<br />
@@ -65,6 +113,23 @@ function FocusInput({ location }) {
 							Let's build a search bar in React!
 						</a>
 					</p>
+				</div>
+
+				<div className="show__item" style={{marginBottom: '4rem'}}>
+					{isDetail ? (
+						<div className={`show__item__detail ${isClass}`}>text text texttexttexttexttexttext text texttexttexttexttexttext text texttexttexttexttexttext text texttexttexttexttext</div>
+					) : (
+						<button className="show__item__button" onClick={handleDisplay}>
+							Read More
+						</button>
+					)}
+				</div>
+
+				<div>
+					<div id="my-elem" style={{width: '300px', height: '200px', background: 'gray'}}>
+						{/* {i} */}
+						<input value="0" />
+					</div>
 				</div>
 			</StyledWrap>
 		</Layout>
