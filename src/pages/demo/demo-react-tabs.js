349cd353ab9041fe1294d29cb9e2a50100552a6d
Ayana Osawa <ayana.osawa@gmail.com>
2020-07-15 15:56:38 -0400

    Add localStrage, update react tabs

---
+++ src/pages/demo/demo-react-tabs.js
@@ -0,0 +1,80 @@
+import React, {useState} from 'react'
+import Layout from '../../components/Layout'
+import SEO from '../../components/SEO'
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs'
+import 'react-tabs/style/react-tabs.css'
+import styled from 'styled-components'
+
+const StyledDemo = styled.div`
+	.panel {
+		min-height: 50vh;
+	}
+`
+
+const DemoTab = ({location}) => {
+	const pageTitle = 'Demo Tab UI'
+	const pageSlug = 'demo-react-tabs'
+
+	const [value, setValue] = useState('')
+
+	function onChange(e) {
+		localStorage.setItem('item1', e.target.value)
+		setValue(e.target.value)
+	}
+	return (
+		<Layout location={location}>
+			<SEO pageTitle={pageTitle} pageSlug={pageSlug} />
+			<div className='container basic-link'>
+				<StyledDemo>
+					<h1>{pageTitle}</h1>
+					<p>This tab is super simple! No styles!</p>
+					<Tabs>
+						<div className='tabs'>
+							<TabList>
+								<Tab>Tab1</Tab>
+								<Tab>Tab2</Tab>
+								<Tab>Tab3</Tab>
+							</TabList>
+						</div>
+
+						<div className='panel'>
+							<TabPanel>
+								<h2>Panel 1</h2>
+								<div className='item'>
+									<input value={value} onChange={onChange} />
+								</div>
+							</TabPanel>
+							<TabPanel>
+								<h2>Panel 2</h2>
+								<div className='item'>
+									<input value={value} onChange={onChange} />
+								</div>
+							</TabPanel>
+							<TabPanel>
+								<h2>Panel 3</h2>
+								<div className='item'>
+									<input value={value} onChange={onChange} />
+								</div>
+							</TabPanel>
+						</div>
+					</Tabs>
+
+					<p className='ref'>
+						Refference:
+						<br />
+						<a href='https://www.npmjs.com/package/react-tabs' target='_blank' rel='noopener noreferrer'>
+							react-tabs
+						</a>{' '}
+						<br />
+						<a href='https://github.com/jeetiss/tabs' target='_blank' rel='noopener noreferrer'>
+							@bumaga/tabs
+						</a>{' '}
+						→ Switched react-tabs seeing bug issue of (https://github.com/jeetiss/tabs/issues/122)
+					</p>
+				</StyledDemo>
+			</div>
+		</Layout>
+	)
+}
+
+export default DemoTab
--- src/pages/demo/demo-redux-form.js
+++ src/pages/demo/demo-redux-form.js
@@ -1,9 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import Layout from '../../components/Layout'
 import SEO from '../../components/SEO'
-import {format} from 'date-fns'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
 
 import styled from 'styled-components'
 
@@ -34,42 +31,9 @@ const StyledItem = styled.div`
 	}
 `
 
-const DemoDate = (props) => {
-	const location = props.location
-	const pageTitle = 'Demo Date'
-	const pageSlug = 'demo-date'
-
-	// item1 - regular date
-	// const [item1DateDD, setItem1DateDD] = useState('')
-	// const [item1DateMM, setItem1DateMM] = useState('')
-	const [item1DateYear, setItem1DateYear] = useState('')
-
-	// item2 - react datepicker
-	const [item2SelectDate, setItem2SelectDate] = useState(new Date())
-	const [item2DateValue, setItem2DateValue] = useState('Pick a date above')
-
-	function getCurrentDate() {
-		const selectMonth = document.querySelector('#select-date-mm')
-		const selectDate = document.querySelector('#select-date-dd')
-
-		const today = new Date()
-		const year = today.getFullYear()
-		const month = ('0' + (today.getMonth() + 1)).slice(-2)
-		const date = ('0' + today.getDate()).slice(-2)
-
-		setItem1DateYear(year)
-		// setItem1DateMM(month)
-		// setItem1DateDD(date)
-
-		selectMonth.selectedIndex = month
-		selectDate.selectedIndex = date
-	}
-
-	function handleDateChange(item2SelectDate) {
-		const formatDate = format(item2SelectDate, 'yyyy/MM/dd')
-		setItem2DateValue(formatDate)
-		setItem2SelectDate(item2SelectDate)
-	}
+const DemoReduxForm = ({location}) => {
+	const pageTitle = 'Demo Redux Form'
+	const pageSlug = 'demo-redux-form'
 
 	useEffect(() => {
 		// item3 - loop date
@@ -109,11 +73,8 @@ const DemoDate = (props) => {
 					{/* item1 */}
 					<div className='item' style={{marginBottom: '5rem'}}>
 						<h2>Static Date</h2>
-						<button className='button-today' onClick={getCurrentDate}>
-							Today
-						</button>
-						<input id='input-date-year' className='width-s' type='text' placeholder='Year' value={item1DateYear} onChange={(e) => setItem1DateYear(e.target.value)} />
-						<select id='select-date-mm' className='width-s mar-l-10' onChange={(e) => e.target.value}>
+						<input id='input-date-year' className='width-s' type='text' placeholder='Year' />
+						<select id='select-date-mm' className='width-s mar-l-10'>
 							<option value=''>MM</option>
 							<option value='01'>1</option>
 							<option value='02'>2</option>
@@ -128,7 +89,7 @@ const DemoDate = (props) => {
 							<option value='11'>11</option>
 							<option value='12'>12</option>
 						</select>
-						<select id='select-date-dd' className='width-s mar-l-10' onChange={(e) => e.target.value}>
+						<select id='select-date-dd' className='width-s mar-l-10'>
 							<option value=''>DD</option>
 							<option value='01'>1</option>
 							<option value='02'>2</option>
@@ -179,31 +140,7 @@ const DemoDate = (props) => {
 						<p className='ref' style={{marginTop: '3rem'}}>
 							Refference:
 							<br />
-							<a href='https://its-office.jp/blog/js/2016/03/08/ymd-html.html' target='_blank' rel='noopener noreferrer'>
-								簡単にHTMLの年月日のプルダウンを作る方法
-							</a>
-							<br />
-							<a href='https://blog.mimipen.net/entry/2019/09/02/083834' target='_blank' rel='noopener noreferrer'>
-								Next Step: JavaScriptで日付セレクトボックスを作ろう！～うるう年も考慮してみよう！～
-							</a>
-						</p>
-					</div>
-
-					{/* item2 */}
-					<div className='item' style={{marginBottom: '5rem'}}>
-						<h2>React Date Picker with date-fns</h2>
-						<DatePicker className='element' selected={item2SelectDate} onChange={handleDateChange} dateFormat='MM/dd/yyyy h:mm aa' placeholderText='Click to pick date' /> <br />
-						<p style={{marginTop: '1rem', fontSize: '0.9rem'}}>Custom Value: {item2DateValue}</p>
-						<p className='ref' style={{marginTop: '3rem'}}>
-							Refference:
-							<br />
-							<a href='https://reactdatepicker.com/' target='_blank' rel='noopener noreferrer'>
-								React Datepicker
-							</a>
-							<br />
-							<a href='https://date-fns.org/' target='_blank' rel='noopener noreferrer'>
-								date-fns
-							</a>
+							<a href='https://its-office.jp/blog/js/2016/03/08/ymd-html.html' target='_blank' rel='noopener noreferrer'></a>
 						</p>
 					</div>
 				</div>
@@ -212,4 +149,4 @@ const DemoDate = (props) => {
 	)
 }
 
-export default DemoDate
+export default DemoReduxForm
